c473e29236136401457a62c9f787941e7ec3552d
skydefediltam <matteo.defedilta.reply@skytv.it>
2020-11-23 17:25:07 +0100

    first components

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -9,7 +9,15 @@ export class ProgressBarComponent implements OnInit {
 
   constructor() { }
 
+  progressValue: number;
+  daysCount:number;
+
   ngOnInit(): void {
+    let currentDate:any = new Date();
+    let TDay:any = new Date("October, 28, 2021");
+    let DayCount = (TDay - currentDate) / (1000 * 60 * 60 * 24);
+    this.daysCount = Math.round(DayCount);
+    this.progressValue = 100-(DayCount*100/365);
   }
 
 }
