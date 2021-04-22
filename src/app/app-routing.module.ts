a2442534f1334685ee605cefaab3862ccb6c961f
m.defedilta <m.defedilta@reply.it>
2021-04-22 17:40:01 +0200

    creata lettura utente del ticket

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { CardsComponent } from './cards/cards.component';
 import { CardPageComponent } from './card-page/card-page.component';
 import { LoginPageComponent } from './authentication/login-page/login-page.component';
 import { AuthGuard } from './authentication/service/auth-guard.service';
+import { TicketComponent } from './ticket/ticket.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'card', component: CardsComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginPageComponent },
+  { path: 'ticket', component: TicketComponent },
   { path: ':id', component: CardPageComponent },
 ];
 @NgModule({
